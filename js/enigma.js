9d44823fab909cf7f81cffdc82ba1202286de517
ElReyPato <Lucaslibmar@gmail.com>
2024-02-05 21:13:07 +0100

    Update enigma.js

--- js/enigma.js
+++ js/enigma.js
@@ -1,8 +1,7 @@
 function obtenerValorFormulario() {
     // Obtén el valor del campo de texto
     const contraseña = document.getElementById('contraseña').value;
-    const pass = contraseña.toLowerCase()
-    alert (pass);
+    const pass = contraseña.toLowerCase();
     if (contraseña == "whynout"){
         alert("Casi casi no estas lejos.")
     }else if (contraseña != "wobbuffet"){
